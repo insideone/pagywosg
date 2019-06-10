c7852c9dce069ff60695fe59fb92c9371f1da8b8
insideone <xim.nenko@gmail.com>
2019-06-10 03:08:24 +0300

    - https support

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 1. Enter the `docker` group (https://docs.docker.com/install/linux/linux-postinstall/ **Manage Docker as a non-root user** section)
 1. Run `./from-scratch.sh` in project directory and follow instructions
 1. Ensure that project is up by looking at `docker-compose ps` command response in parallel command line. All states must be `Up`. If they're not, please run `docker-compose up <container>` where `<container>` is the container which isn't Up. Put results to the new [Issue](TODO:link_to_new_issue)
-1. When all is Up you can open your project instance in browser: http://play-a-game.localhost/
+1. When all is Up you can open your project instance in browser: http://play-a-game.localhost/ or via https://play-a-game.localhost/
 
 # Regular start
 
--- from-scratch.sh
+++ from-scratch.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+set -e
+
 hr () {
-  echo '--------------------------------------';
+  HR='======================================'
+  if [[ "$#" -gt 0 ]]; then
+    echo "/$HR\\";
+    echo "   $@"
+    echo "\\$HR/";
+  else
+    echo "$HR";
+  fi
 }
 
 hr
@@ -43,11 +52,36 @@ if [[ "$REWRITE" =  "Y" ]]; then
     TZ=${TZ:-Europe/London}
     sed -i "s|TZ=|TZ=$TZ|g" .env.local
 
-    hr && echo "We will need a Steam API key to proceed: " && hr
+    hr "We will need a Steam API key to proceed: "
     read STEAM_API_KEY
     sed -i "s|STEAM_API_KEY=|STEAM_API_KEY=$STEAM_API_KEY|g" .env.local
 fi
 
-hr && echo "docker-compose finally..." && hr
+
+HTTPS_CERTIFICATE_DIRECTORY=./env/https
+HTTPS_CERTIFICATE_KEY_FILE="$HTTPS_CERTIFICATE_DIRECTORY/key.pem"
+HTTPS_CERTIFICATE_CERT_FILE="$HTTPS_CERTIFICATE_DIRECTORY/cert.pem"
+if [[ -f "$HTTPS_CERTIFICATE_KEY_FILE" ]]; then
+    hr
+    echo "Should we rewrite existed https certificate?"
+    echo "Press <Enter> to keep or <Y><Enter> to rewrite"
+    hr
+    read WRITE_CERTIFICATE
+else
+    WRITE_CERTIFICATE=Y
+fi
+
+WRITE_CERTIFICATE=${WRITE_CERTIFICATE:-N}
+
+if [[ ! -d "$HTTPS_CERTIFICATE_DIRECTORY" ]]; then
+    mkdir "$HTTPS_CERTIFICATE_DIRECTORY"
+fi
+
+if [[ "$WRITE_CERTIFICATE" = "Y" ]]; then
+    hr "Generating https certificate"
+    openssl req -subj '/CN=localhost' -x509 -newkey rsa:4096 -nodes -keyout "$HTTPS_CERTIFICATE_KEY_FILE" -out "$HTTPS_CERTIFICATE_CERT_FILE" -days 365
+fi
+
+hr "docker-compose finally..."
 
 FROM_SCRATCH=Y ./up
