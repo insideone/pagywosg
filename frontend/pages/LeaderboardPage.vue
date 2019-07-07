c3013987b3aed06a6f14a81fce4dafefb98af554
Adriffaz <ardiffaz@gmail.com>
2019-07-07 16:59:26 +0300

    #24 eligible for rewards people list

--- frontend/pages/LeaderboardPage.vue
+++ frontend/pages/LeaderboardPage.vue
@@ -12,91 +12,105 @@
                 <i class="icon-fa icon-fa--inline fas fa-crown"></i>{{event.name}}
             </router-link>
 
-            <table class="leaderboard">
-                <tr>
-                    <th class="leaderboard__th leaderboard__num">#</th>
-                    <th class="leaderboard__th">
-                        <i class="icon-fa icon-fa--inline fas fa-user"></i>
-                        Username
-                    </th>
-                    <th class="leaderboard__th">
-                        <i class="icon-fa icon-fa--inline fas fa-trophy"></i>
-                        Number of Achievements
-                    </th>
-                    <th class="leaderboard__th">
-                        <i class="icon-fa icon-fa--inline fas fa-clock"></i>
-                        Hours of Playtime
-                    </th>
-                    <th class="leaderboard__th">
-                        <i class="icon-fa icon-fa--inline fas fa-gamepad"></i>
-                        Games Beaten
-                    </th>
-                </tr>
-                <tr
-                    v-for="(lbEntry, key) in leaderboard"
-                    :key="'lb_'+key"
-                    :class="{
-                        'leaderboard__tr-inactive': (lbEntry.beaten <= 0),
-                        'leaderboard__tr-mine': loggedUserId === users[lbEntry.player].id
-                    }"
-                >
-                    <td class="leaderboard__td leaderboard__num">{{key+1}}</td>
-                    <td class="leaderboard__td leaderboard__user">
-                        <div class="leaderboard__username">
+            <div class="lb-page__flex-cont">
+                <table class="leaderboard lb-page__main-table">
+                    <tr>
+                        <th class="leaderboard__th leaderboard__num">#</th>
+                        <th class="leaderboard__th">
+                            <i class="icon-fa icon-fa--inline fas fa-user"></i>
+                            Username
+                        </th>
+                        <th class="leaderboard__th">
+                            <i class="icon-fa icon-fa--inline fas fa-trophy"></i>
+                            Number of Achievements
+                        </th>
+                        <th class="leaderboard__th">
+                            <i class="icon-fa icon-fa--inline fas fa-clock"></i>
+                            Hours of Playtime
+                        </th>
+                        <th class="leaderboard__th">
+                            <i class="icon-fa icon-fa--inline fas fa-gamepad"></i>
+                            Games Beaten
+                        </th>
+                    </tr>
+                    <tr
+                        v-for="(lbEntry, key) in leaderboard"
+                        :key="'lb_'+key"
+                        :class="{
+                            'leaderboard__tr-inactive': (lbEntry.beaten <= 0),
+                            'leaderboard__tr-mine': loggedUserId === users[lbEntry.player].id
+                        }"
+                    >
+                        <td class="leaderboard__td leaderboard__num">{{key+1}}</td>
+                        <td class="leaderboard__td leaderboard__user">
+                            <div class="leaderboard__username">
+                                <router-link
+                                    :to="{name: 'user_profile', params: {userId: lbEntry.player}}"
+                                >{{users[lbEntry.player].sgProfileName ?
+                                    users[lbEntry.player].sgProfileName : users[lbEntry.player].profileName}}</router-link>
+                            </div>
+                            <div class="leaderboard__userlinks">
+                                <a
+                                    :href="'https://steamcommunity.com/profiles/'+users[lbEntry.player].steamId+'/'"
+                                    target="_blank"
+                                    class="leaderboard__user-link"
+                                >
+                                    <i class="icon-fa icon-fa--inline fab fa-steam-symbol"></i>Steam
+                                </a>
+                                <a
+                                    :href="'http://steamgifts.com/go/user/'+users[lbEntry.player].steamId"
+                                    target="_blank"
+                                    class="leaderboard__user-link"
+                                >
+                                    <i class="icon-fa icon-fa--inline far fa-square"></i>Steamgifts
+                                </a>
+                            </div>
+                        </td>
+                        <td class="leaderboard__td">{{+lbEntry.achievements}}</td>
+                        <td class="leaderboard__td">
+                            <span
+                                :title="calcPreciseTime( lbEntry.minutes )"
+                            >{{+lbEntry.hours}}</span>
+                        </td>
+                        <td class="leaderboard__td">
                             <router-link
-                                :to="{name: 'user_profile', params: {userId: lbEntry.player}}"
-                            >{{users[lbEntry.player].sgProfileName ?
-                                users[lbEntry.player].sgProfileName : users[lbEntry.player].profileName}}</router-link>
-                        </div>
-                        <div class="leaderboard__userlinks">
-                            <a
-                                :href="'https://steamcommunity.com/profiles/'+users[lbEntry.player].steamId+'/'"
-                                target="_blank"
-                                class="leaderboard__user-link"
-                            >
-                                <i class="icon-fa icon-fa--inline fab fa-steam-symbol"></i>Steam
-                            </a>
-                            <a
-                                :href="'http://steamgifts.com/go/user/'+users[lbEntry.player].steamId"
-                                target="_blank"
-                                class="leaderboard__user-link"
-                            >
-                                <i class="icon-fa icon-fa--inline far fa-square"></i>Steamgifts
-                            </a>
-                        </div>
-                    </td>
-                    <td class="leaderboard__td">{{+lbEntry.achievements}}</td>
-                    <td class="leaderboard__td">
-                        <span
-                            :title="calcPreciseTime( lbEntry.minutes )"
-                        >{{+lbEntry.hours}}</span>
-                    </td>
-                    <td class="leaderboard__td">
-                        <router-link
-                            :to="{
-                                name: 'event_detail',
-                                params: {eventId: event.id},
-                                query: {
-                                    player: users[lbEntry.player].steamId,
-                                    status: 'b_c'
-                                }
-                            }"
-                        >{{+lbEntry.beaten}}</router-link>
+                                :to="{
+                                    name: 'event_detail',
+                                    params: {eventId: event.id},
+                                    query: {
+                                        player: users[lbEntry.player].steamId,
+                                        status: 'b_c'
+                                    }
+                                }"
+                            >{{+lbEntry.beaten}}</router-link>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="leaderboard__td leaderboard__total-num">Total:</td>
+                        <td class="leaderboard__td leaderboard__total">{{leaderboard.length}} people</td>
+                        <td class="leaderboard__td leaderboard__total">{{totalStats.achievements}}</td>
+                        <td class="leaderboard__td leaderboard__total">
+                            <span
+                                :title="calcPreciseTime( totalStats.minutes )"
+                            >{{totalStats.hours}}</span>
+                        </td>
+                        <td class="leaderboard__td leaderboard__total">{{totalStats.beaten}}</td>
+                    </tr>
+                </table>
 
-                    </td>
-                </tr>
-                <tr>
-                    <td class="leaderboard__td leaderboard__total-num">Total:</td>
-                    <td class="leaderboard__td leaderboard__total">{{leaderboard.length}} people</td>
-                    <td class="leaderboard__td leaderboard__total">{{totalStats.achievements}}</td>
-                    <td class="leaderboard__td leaderboard__total">
-                        <span
-                            :title="calcPreciseTime( totalStats.minutes )"
-                        >{{totalStats.hours}}</span>
-                    </td>
-                    <td class="leaderboard__td leaderboard__total">{{totalStats.beaten}}</td>
-                </tr>
-            </table>
+                <div class="lb-page__eligible-list">
+                    <div
+                        v-if="showTemplate"
+                        class="form lb-page__template"
+                    >
+                        <label for="lb-entrants" class="form__label">List of people eligible for event rewards:</label>
+                        <textarea
+                            id="lb-entrants"
+                            class="form__textarea"
+                        >{{eligiblePeopleList}}</textarea>
+                    </div>
+                </div>
+            </div>
 
             <div
                 v-if="showTemplate"
@@ -188,6 +202,22 @@
                 mdTemplate += `**Total** | **${+this.totalStats.achievements}** | **${+this.totalStats.hours}** | **${+this.totalStats.beaten}**\n`;
 
                 return mdTemplate;
+            },
+
+            eligiblePeopleList: function() {
+                let peopleListTemplate = '';
+
+                this.leaderboard.forEach((lbEntry) => {
+                    if (lbEntry.beaten <= 0)
+                        return true;
+
+                    let user = this.users[lbEntry.player];
+                    let userName = user.sgProfileName ? user.sgProfileName : '';
+
+                    peopleListTemplate += `${userName} - ${user.steamId}\n`;
+                });
+
+                return peopleListTemplate;
             }
         },
         created() {
@@ -217,6 +247,22 @@
 
         &__template{
             max-width: 800px;
+        }
+
+        &__flex-cont{
+            display: flex;
+
+            @media (max-width: 1200px) {
+                display: block;
+            }
+        }
+
+        &__main-table{
+            margin-right: 40px;
+        }
+
+        &__eligible-list{
+            margin-bottom: 30px;
         }
     }
 </style>
